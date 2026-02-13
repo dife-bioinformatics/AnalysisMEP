290b2799cd77122b44b51d80c30686a7b93859a7
FlorianSchw <Florian.Schwarz@dife.de>
2026-02-13 20:29:50 +0100

    style: changed style to adhere as much as possible to tidyverse style using the lintr package. currently only being tested on the files within the R folder and no other ".R" files

--- R/initialize_project.R
+++ R/initialize_project.R
@@ -1,16 +1,21 @@
 #'
 #' @title Function to initiate a new project
-#' @description The function will create a new analysis project with pre-specified folders and files.
-#' `r lifecycle::badge('experimental')`
-#' @details The function initiates folders and files that demonstrate good practise, such as R folder with R files
-#' in them. Furthermore, for the data, data-raw, results/figures and results/tables folders dummy files have been added
-#' to keep the folder structure when collaborating with others on GitHub because those folders have otherwise been set to
-#' be ignored by git (see .gitignore file). In the README.md file a very brief description of the project should be provided.
-#' @param path defaults to "home". The function uses the `fs::path_expand()` functionality to find the path
-#' to the home directory using the tilde expression. When this argument is changed, it
-#' should be replaced with an absolute path.
+#' @description The function will create a new analysis project with pre-
+#' specified folders and files.`r lifecycle::badge('experimental')`
+#' @details The function initiates folders and files that demonstrate good
+#' practise, such as R folder with R files in them. Furthermore, for the data,
+#' data-raw, results/figures and results/tables folders dummy files have been
+#' added to keep the folder structure when collaborating with others on GitHub
+#' because those folders have otherwise been set to be ignored by git
+#' (see .gitignore file). In the README.md file a very brief description of the
+#' project should be provided.
+#' @param path defaults to "home". The function uses the `fs::path_expand()`
+#' functionality to find the path to the home directory using the tilde
+#' expression. When this argument is changed, it should be replaced with an
+#' absolute path.
 #' @param name name of the new R project (and folder)
-#' @return A new project will be set up at the specified location (path + name) by the user.
+#' @return A new project will be set up at the specified location
+#' (path + name) by the user.
 #' @author Florian Schwarz, Fabian Eichelmann
 #' @import renv
 #' @import fs
@@ -18,26 +23,29 @@
 #' @export
 #'
 
-initialize_project <- function(path = "home", name = NULL){
+initialize_project <- function(path = "home", name = NULL) {
 
   lifecycle::signal_stage(stage = "experimental")
 
-  if(is.null(name)){
-    stop("Please provide a path name for the project to be created.", call. = FALSE)
+  if (is.null(name)) {
+    stop("Please provide a path name for the project to be created.",
+         call. = FALSE)
   }
 
-  if(path == "home"){
+  if (path == "home") {
 
-      new_project_path <- paste0(fs::path_expand("~"),"/",name)
+    new_project_path <- paste0(fs::path_expand("~"), "/", name)
 
   } else {
 
-      new_project_path <- paste0(path,"/", name)
+    new_project_path <- paste0(path, "/", name)
 
   }
 
-  if(fs::dir_exists(new_project_path)){
-    stop(paste0("The path and name you have provided would overwrite an existing directory (", new_project_path , "). Setup aborted."), call. = FALSE)
+  if (fs::dir_exists(new_project_path)) {
+    stop(paste0("The path and name you have provided would overwrite an existing
+                directory (", new_project_path, "). Setup aborted."),
+         call. = FALSE)
   }
 
   #### sets up normal R Project
@@ -56,37 +64,43 @@ initialize_project <- function(path = "home", name = NULL){
 
   #### copies over standardised R scripts for start
   file.copy(from = int_find_script("scripts/01_data_management.R"),
-            to = paste0(new_project_path, "/R/01_data_management.R"))
+            to = paste0(new_project_path,
+                        "/R/01_data_management.R"))
+
   file.copy(from = int_find_script("scripts/example_script.R"),
-            to = paste0(new_project_path, "/R/02_example.R"))
+            to = paste0(new_project_path,
+                        "/R/02_example.R"))
+
   file.copy(from = int_find_script("scripts/custom_functions.R"),
-            to = paste0(new_project_path, "/R/utils/custom_function_example.R"))
+            to = paste0(new_project_path,
+                        "/R/utils/custom_function_example.R"))
 
-  #### copies over README.md file to have a small description of the project / repo
+  #### copies over README.md file for a small description of the project
   file.copy(from = int_find_script("utils/README.md"),
-            to = paste0(new_project_path, "/README.md"))
+            to = paste0(new_project_path,
+                        "/README.md"))
 
-  #### copies over placeholder files to keep folder structure in place for GitHub
-  #### for folders that should not be shared (e.g. results)
+  #### copies over placeholder to keep folder structure in place for GitHub
+  #### for folders that should not be shared (e.g. data, results)
   file.copy(from = int_find_script("utils/placeholder.txt"),
-            to = paste0(new_project_path, "/results/tables/aim_for_machine_readable.txt"))
-  file.copy(from = int_find_script("utils/placeholder.txt"),
-            to = paste0(new_project_path, "/results/figures/aim_for_png.txt"))
-  file.copy(from = int_find_script("utils/placeholder.txt"),
-            to = paste0(new_project_path, "/data/all_processed_data.txt"))
-  file.copy(from = int_find_script("utils/placeholder.txt"),
-            to = paste0(new_project_path, "/data-raw/only_raw_data.txt"))
-  file.copy(from = int_find_script("utils/placeholder.txt"),
-            to = paste0(new_project_path, "/citations/citations_placeholder_file.txt"))
+            to = paste0(new_project_path,
+                        "/results/tables/aim_for_machine_readable.txt"))
 
-  #### copies over initial config.yml file
-  #### not sure if we should keep this but it is extremely helpful for checking out to improve figures
-  #### could be used to determine the theme in ggplot e.g.
-  #### could also be used in connection with how to flexibly merge plots together into one figure
-  #### set fixed fonts throughout
-  # file.copy(from = find_script("utils/config.yml"),
-  #           to = paste0(new_project_path, "/config.yml"))
+  file.copy(from = int_find_script("utils/placeholder.txt"),
+            to = paste0(new_project_path,
+                        "/results/figures/aim_for_png.txt"))
 
+  file.copy(from = int_find_script("utils/placeholder.txt"),
+            to = paste0(new_project_path,
+                        "/data/all_processed_data.txt"))
+
+  file.copy(from = int_find_script("utils/placeholder.txt"),
+            to = paste0(new_project_path,
+                        "/data-raw/only_raw_data.txt"))
+
+  file.copy(from = int_find_script("utils/placeholder.txt"),
+            to = paste0(new_project_path,
+                        "/citations/citations_placeholder_file.txt"))
 
   #### modify .gitignore file
   gitignore_lines <- c("",
@@ -104,24 +118,17 @@ initialize_project <- function(path = "home", name = NULL){
                        "results/tables/*",
                        "!results/tables/placeholder.txt")
 
-  usethis::write_union(path = paste0(new_project_path, "/.gitignore"),
+  usethis::write_union(path = paste0(new_project_path,
+                                     "/.gitignore"),
                        lines = gitignore_lines)
 
 
-  #### initiate and fill standard .Renviron file
-
-  # r_environ_lines <- c("R_CONFIG_ACTIVE = 'default'")
-  #
-  # usethis::write_over(path = paste0(new_project_path, "/.Renviron"),
-  #                     lines = r_environ_lines)
-
   r_profile_lines <- c("source(\"renv/activate.R\")",
-                       "purrr::walk(fs::dir_ls(here::here(\"R/utils\")), ~source(.))")
+                       "purrr::walk(fs::dir_ls(here::here(\"R/utils\")), ~source(.))") # nolint
 
-  usethis::write_over(path = paste0(new_project_path, "/.Rprofile"),
+  usethis::write_over(path = paste0(new_project_path,
+                                    "/.Rprofile"),
                       lines = r_profile_lines)
-
-  # renv::install("survival")
 
   renv::init(project = new_project_path,
              load = FALSE)
